fd68d1bd9d6aff1b7c50b88bc29edef982ce9160
lvzhengyang <lvzhengyang@mail.ustc.edu.cn>
2026-05-30 16:19:05 +0800

    add cook

--- dv/bsdcov/scripts/flow.sh
+++ dv/bsdcov/scripts/flow.sh
@@ -2,3 +2,4 @@
 ./scripts/extract.sh
 ./scripts/rand_instr.sh --seed 1 --num 300 --chunk-size 100 --force
 ./scripts/launch_sim.sh   --instr-seq riscvdv/assembly/seq.1.300.chunks.f   --bind-flist bsdcovproj/sim_bind.f   --cov-update 100   --jobs 3
+./scripts/cook.sh
