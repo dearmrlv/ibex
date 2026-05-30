7576718c85f6678f5789c3446235ac3fbd8f574a
lvzhengyang <lvzhengyang@mail.ustc.edu.cn>
2026-05-30 22:57:48 +0800

    add env var

--- dv/bsdcov/scripts/cook.sh
+++ dv/bsdcov/scripts/cook.sh
@@ -4,6 +4,7 @@ set -euo pipefail
 BSDCOV_DIR="$(cd "$(dirname "$0")/.." && pwd)"
 ROOT="$(cd "$BSDCOV_DIR/../../../.." && pwd)"
 
+BSDCOV_JG=/home/lvzhengyang/workspace/cadence/jg \
 python3 "$ROOT/main.py" bsdcov cook \
   --force \
   "$BSDCOV_DIR/bsdcovproj"
