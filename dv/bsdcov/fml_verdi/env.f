6a17ad28ef8e53ba7829d30680e41d0e90224b69
lvzhengyang <lvzhengyang@mail.ustc.edu.cn>
2026-06-04 20:32:33 +0800

    add verdi launch scripts

---
+++ dv/bsdcov/fml_verdi/env.f
@@ -0,0 +1,3 @@
+../fml/fv_env/instr_mem.sv
+../fml/fv_env/instr_mem_assumptions.sv
+../fml/fv_env/instr_legality.sv
---
+++ dv/bsdcov/fml_verdi/launch_verdi.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+IBEX_PARAMS="\
+  -pvalue+ibex_top.RV32E=0 \
+  -pvalue+ibex_top.RV32M=ibex_pkg::RV32MSingleCycle \
+  -pvalue+ibex_top.RV32B=ibex_pkg::RV32BOTEarlGrey \
+  -pvalue+ibex_top.RV32ZC=ibex_pkg::RV32ZcaZcbZcmp \
+  -pvalue+ibex_top.RegFile=ibex_pkg::RegFileFF \
+  -pvalue+ibex_top.BranchTargetALU=1 \
+  -pvalue+ibex_top.WritebackStage=1 \
+  -pvalue+ibex_top.ICache=1 \
+  -pvalue+ibex_top.ICacheECC=1 \
+  -pvalue+ibex_top.ICacheScramble=1 \
+  -pvalue+ibex_top.ICacheTweakInfection=0 \
+  -pvalue+ibex_top.BranchPredictor=0 \
+  -pvalue+ibex_top.DbgTriggerEn=1 \
+  -pvalue+ibex_top.SecureIbex=1 \
+  -pvalue+ibex_top.PMPEnable=1 \
+  -pvalue+ibex_top.PMPGranularity=0 \
+  -pvalue+ibex_top.PMPNumRegions=16 \
+  -pvalue+ibex_top.MHPMCounterNum=10 \
+  -pvalue+ibex_top.MHPMCounterWidth=32 \
+  -pvalue+ibex_top.DbgHwBreakNum=1 \
+  -pvalue+ibex_top.DmBaseAddr=32'h1A110000 \
+  -pvalue+ibex_top.DmAddrMask=32'h00000FFF \
+  -pvalue+ibex_top.DmHaltAddr=32'h80000000 \
+  -pvalue+ibex_top.DmExceptionAddr=32'h80000008 \
+  -pvalue+ibex_top.BootAddr=32'h80000000 \
+"
+
+/home/lvzhengyang/workspace/synopsys/verdi/verdi-raw \
+  -2012 \
+  -f dut.f \
+  -f ../bsdcovproj/results/bsdcov.f \
+  -f env.f \
+  -top ibex_top \
+  $IBEX_PARAMS \
+  -ssf \
+/home/lvzhengyang/workspace/BSD-Cov/designs/ibex/dv/bsdcov/fml/jgproject/traces_fsdb.bsd_cov_region_asserts/4.gions_stall_mem_stall_multdiv_stall_branch_stall_jump_c815.AST_BSDCOV_stall_mem_stall_multdiv_stall_branch_stall_jump_c815_R_unique_010.Ht.6.fsdb
