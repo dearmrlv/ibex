74f35d2b59c0600e756e2e889ce4213dd953c5be
dearmrlv <40029891+dearmrlv@users.noreply.github.com>
2026-06-04 14:27:25 +0800

    Use full DUT hierarchy for BSD-Cov cone layout

--- dv/bsdcov/scripts/extract.sh
+++ dv/bsdcov/scripts/extract.sh
@@ -6,7 +6,7 @@ ROOT="$(cd "$BSDCOV_DIR/../../../.." && pwd)"
 
 python3 "$ROOT/main.py" bsdcov extract \
   --module ibex_id_stage \
-  --dut-inst-hier u_ibex_core.id_stage_i \
+  --dut-inst-hier ibex_top.u_ibex_core.id_stage_i \
   --output-signals stall_mem,stall_multdiv,stall_branch,stall_jump \
   --force \
   "$BSDCOV_DIR/bsdcovproj"
